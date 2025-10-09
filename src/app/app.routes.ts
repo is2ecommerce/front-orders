f7bc686d97fbf420fc95c5dcb9fb4e4c775b78d0
Juli615 <julianuribe08@hotmail.com>
2025-10-08 23:35:53 -0500

    vista ordenes, falta navbar y pulir otras cosas, para el proximo sprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { OrderHistory } from './components/order-history/order-history';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'historial-ordenes', component: OrderHistory },
+  { path: '', redirectTo: '/historial-ordenes', pathMatch: 'full' } // Redirige por defecto
+];
